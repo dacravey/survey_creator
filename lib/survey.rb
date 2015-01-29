7b0ece57a68b36fe2026ba6655dc8bfe4c5c8e0f
Debbie Cravey & Lizzie Koehler <student@epicodus.com>
2015-01-28 16:03:10 -0800

    validation specs and tests

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,11 +1,12 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
+  validates(:survey_name, :presence => true)
   before_save(:capitalize_survey_name)
 
 private
 
   define_method(:capitalize_survey_name) do
-    self.survey_name=(survey_name().capitalize())
+    self.survey_name=(survey_name().capitalize!())
   end
 
 end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -7,4 +7,9 @@ describe(Survey) do
     survey_name = Survey.create({:survey_name => "survey"})
     expect(survey_name.survey_name()).to eq("Survey")
   end
+
+  it ("validates the presence of a Survey name") do
+    survey = Survey.new({:survey_name => ""})
+    expect(survey.save()).to(eq(false))
+  end
 end
